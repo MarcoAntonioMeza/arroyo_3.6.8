106bbbf38e22a924f929ecf5b43a4ca121eeacea
antonio <marcosmeza01@outlook.com>
2025-01-28 11:26:35 -0600

    mejoras prod

--- arroyo/urls.py
+++ arroyo/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
      path('',                           login_required(v.index), name='ventas_index'),
      path('mes/',                       login_required(v.ventas_mes), name='ventas_mes'),
      path('productos/',                 login_required(v.productos), name='producto_detalle'),
-     path('productos/mesnos-vendidos/', login_required(v.productos_menos_vendidos), name='producto_menos_vendidos'),
+     path('productos/menos-vendidos/', login_required(v.productos_menos_vendidos), name='producto_menos_vendidos'),
      path('productos/venta-compra',     login_required(v.producto_compra_ventas), name='producto_venta_compra'),
      
      
